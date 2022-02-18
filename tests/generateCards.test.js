cbefe7198c561fc980f07d6312699bc5117e2c60
jeremy0anderson <projects@jeremyanderson.dev>
2022-02-18 08:07:37 -0700

    added test for the rest of the types/roles of employees and generating HTML based on user input/employee role

--- tests/generateCards.test.js
+++ tests/generateCards.test.js
@@ -22,5 +22,41 @@ describe("Cards", () => {
                                 </section>
                             </div>`
             });
+            const newEngineer = new Engineer('Engineer 1', '12345', 'email@engineer.com', 'GitHubUsername');
+            expect(new Card(newEngineer)).toEqual({
+                role:"Engineer", card: `<div class="card" id="engineer-12345">
+                                <h3 class="card-header">Engineer 1 / Engineer</h3>
+                                <section class="content-section">
+                                    <div>
+                                        <h4>Employee ID:</h4>
+                                        <p>12345</p>
+                                    </div>
+                                        <h4>Email:</h4>
+                                        <p>email@engineer.com</p>
+                                    <div>
+                                        <h4>GitHub:</h4>
+                                        <p>GitHubUsername</p>
+                                    </div>
+                                </section>
+                            </div>`
+            });
+            const newManager = new Manager('Manager 1', '12345', 'email@manager.com', '1-800-manager');
+            expect(new Card(newManager)).toEqual({
+                  role: "Manager", card: `<div class="card" id="manager-12345">
+                                <h3 class="card-header">Manager 1 / Manager</h3>
+                                <section class="content-section">
+                                    <div>
+                                        <h4>Employee ID:</h4>
+                                        <p>12345</p>
+                                    </div>
+                                        <h4>Email:</h4>
+                                        <p>email@manager.com</p>
+                                    <div>
+                                        <h4>Office Number:</h4>
+                                        <p>1-800-manager</p>
+                                    </div>
+                                </section>
+                            </div>`
+            })
       })
 })
